6201c3f00c94e188807548cedca2645916baf45d
Leonardo Silva <leonardogabriel8@hotmail.com>
2021-10-26 19:06:00 -0300

    Criando Interface do Projeto

---
+++ src/View/Gelinhos.java
@@ -0,0 +1,53 @@
+package View;
+
+import javax.swing.*;
+
+public class Gelinhos {
+    private JPanel pnlMain;
+    private JTabbedPane tabbedMain;
+    private JPanel pnlAdicionar;
+    private JPanel pnlEstoque;
+    private JPanel pnlVendas;
+    private JLabel lblGelinhos;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+    private JSpinner spinner3;
+    private JSpinner spinner4;
+    private JSpinner spinner5;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JCheckBox checkBox3;
+    private JCheckBox checkBox4;
+    private JCheckBox checkBox5;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JComboBox comboBox1;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JButton button3;
+    private JButton button4;
+    private JSpinner spinner6;
+    private JSpinner spinner7;
+    private JSpinner spinner8;
+    private JSpinner spinner9;
+    private JSpinner spinner10;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JButton button5;
+    private JButton button6;
+
+    public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    frame.setContentPane(new Gelinhos().pnlMain);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    }
+}
